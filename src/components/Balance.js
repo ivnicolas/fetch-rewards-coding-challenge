df55b2d4778374cdc951889ea038c4843eb2aba4
ivnicolas <ikennanicolas@gmail.com>
2021-08-29 09:50:03 -0400

    refactored Balance component

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -5,22 +5,11 @@ class Balance extends Component {
    getTotal(){
        if(localStorage.length !== 0){
         let allTransactions = JSON.parse(localStorage.transactions)
-
-        // allTransactions.map(transaction => points+= transaction.points)
-
-        // allTransactions.reduce((acc,transaction) => acc + transaction.points,0)
-
-        debugger
-        // for(let i = 0 ; i<allTransactions; i++){
-        //     points += JSON.parse(localStorage[localStorage.key(i)]).points
-        // }
-
         return allTransactions.reduce((acc,transaction) => acc + transaction.points,0)
        }else{
            return 0
        }
    }
-
     render() {
         return (
             
