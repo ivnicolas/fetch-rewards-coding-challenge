52d21dec1c075597feec260f354891d8288e5497
ivnicolas <ikennanicolas@gmail.com>
2021-08-29 09:55:04 -0400

    refactored all transactions

--- src/components/allTransactions.js
+++ src/components/allTransactions.js
@@ -2,24 +2,11 @@ import React, { Component } from 'react';
 import {Transaction} from './Transaction';
 
 class allTransactions extends Component {
-    getTransactions(){
-        
-        if(localStorage.length !== 0){
-            let transactions = []
-            for(let i = 0 ; i<localStorage.length ; i++){
-                transactions.push(JSON.parse(localStorage[localStorage.key(i)]))
-            }
-            return transactions
-        }else{
-            return 0
-        }
-    }
-
     render() {
         return (
             <div>
                 <h1> AllTransactions</h1>
-                {this.getTransactions() === 0 ? 'No Transaction' : this.getTransactions().map(transaction =>( <Transaction key={transaction.id} transaction={transaction}/>))}
+                {localStorage.transactions.length === 0 ? 'No Transactions' : JSON.parse(localStorage.transactions).map(transaction =>( <Transaction key={transaction.id} transaction={transaction}/>))}
             </div>
         );
     }
